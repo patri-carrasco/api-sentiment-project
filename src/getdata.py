eb2bdb8677ddb2cfa40a915dfb4d9ee1722b08db
patri-carrasco <patri.carrasco@gmail.com>
2020-11-30 19:43:06 +0100

    Finished getdata

--- src/getdata.py
+++ src/getdata.py
@@ -1,10 +1,5 @@
 from config.configuration import db, col_chat, col_user, col_phrase
 
-def getdata():
-
-  #query = {"name": f'{name}'}
-  #frases = list(collection.find(query,{"_id":0}))
-  return "Hola"
 
 
 def phrasecharacter(character):
@@ -19,3 +14,16 @@ def senti_phrasecharacter(character):
     phrases = list(col_phrase.find(query,{"_id":0, "phrase":1}))
     
     return phrases
+
+
+def show_chat():
+  
+  chats = list(col_chat.find({},{"_id":0, "participants":0}))
+    
+  return chats
+
+
+def show_participants():
+  participantes = list(col_user.find({},{"_id":0}))
+    
+  return participantes
